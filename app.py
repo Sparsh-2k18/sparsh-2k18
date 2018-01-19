b83ffa168a59fd9e7671fc5fcf868af61012c99b
kvnamipara <kvnamipara@gmail.com>
2018-01-19 12:59:27 +0530

    sponser page addition

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def hello():
     return render_template("events.html")
 
+@app.route("/sponser")
+def sponser_page():
+    return render_template("sponser.html")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
