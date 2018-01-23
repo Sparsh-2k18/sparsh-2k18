b74df7b5ab32725986e802880fb2304f400822ed
kvnamipara <kvnamipara@gmail.com>
2018-01-23 08:29:57 +0530

    new UI design

---
+++ static/royal/loader.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+	var body = document.getElementsByTagName('body')[0];
+	var loader = document.getElementById('loader-wrap');
+
+	setTimeout(function() {
+		loader.style.opacity = 0;
+		loader.style.visibility = 'hidden';
+		body.style.overflow = 'visible';
+	},2500);
+}
