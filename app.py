8fc1c2d73fdb7953fd86463e4f31f1bf33689eee
Abhijeet Mathur <abhijeetmathur786@gmail.com>
2018-01-29 23:46:33 +0530

    navigation bar

--- app.py
+++ app.py
@@ -1,69 +1,42 @@
-# app.py
-"""Python script for server implementation."""
-
-from flask import Flask, render_template, request, redirect, jsonify
-from flask import url_for, flash
-from flask_compress import Compress
-
-from sqlalchemy import create_engine, asc, func
-from sqlalchemy.orm import sessionmaker
-from database.database_setup import Base, Contact
-from database.config import get_database_uri
-
-# Connect to Database and create database session
-engine = create_engine(get_database_uri())
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 
 app = Flask(__name__)
-Compress(app)
-
 
 @app.route("/")
 def index():
     return render_template("main.html")
 
-
 @app.route("/index")
 def single_page():
     return render_template("index.html")
-
 
 @app.route("/events")
 def event_page():
     return render_template("new_events.html")
 
+@app.route("/detail")
+def detail_page():
+    return render_template("detail.html")
 
 @app.route("/sponsor")
 def sponsor_page():
     return render_template("sponsor.html")
 
-
 @app.route("/about")
 def about_page():
     return render_template("about.html")
 
-
-@app.route("/winter", methods=['POST'])
+@app.route("/contact")
 def contact_page():
-    if request.method == 'POST':
-        newContact = Contact(name=request.form['name'],
-                             email=request.form['email'],
-                             mobile=request.form['mobile'],
-                             query=request.form['query'])
-        session.add(newContact)
-        session.commit()
-        return redirect(url_for('winter_page'))
-    else:
-        return render_template("royal.html")
+    return render_template("contact.html")
 
-
-@app.route("/winter",methods = ['GET'])
+@app.route("/winter")
 def winter_page():
     return render_template("royal.html")
 
+@app.route("/event_demo")
+def event__page():
+    return render_template("event_demo.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
