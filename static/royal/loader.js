f14d7f0febea67d39c288324b6b8634042fed925
sinwar <premsinwar4@gmail.com>
2018-02-01 16:37:59 +0530

    fix nav bug
    nav does't show now before loading.

--- static/royal/loader.js
+++ static/royal/loader.js
@@ -1,10 +1,15 @@
 window.onload = function() {
 	var body = document.getElementsByTagName('body')[0];
 	var loader = document.getElementById('loader-wrap');
+	var navshow = document.getElementById('navshow');
+
+
 
 	setTimeout(function() {
 		loader.style.opacity = 0;
 		loader.style.visibility = 'hidden';
 		body.style.overflow = 'visible';
+		navshow.style.visibility= 'visible';
+		
 	},1000);
 }
